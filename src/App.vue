7671ecd26bbb83b697f2e0c7a4513e3305edf43c
FeranmiDev <macguzik98@gmail.com>
2024-03-02 17:54:17 +0100

    Add server warm-up functionality

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+const warmServer = async() => {
+  const response = await fetch('https://aps-website-backend.onrender.com/api/v1/gallery/request/dummy');
+  if (!response.ok) {
+    console.log('Server not warmed up');
+  }
+  console.log('Server warmed up');
+}
+warmServer();
+
+setInterval(warmServer, 15*60*1000);
 </script>
 
 <template>
