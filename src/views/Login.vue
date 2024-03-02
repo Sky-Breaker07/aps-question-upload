5d9d614b7de06f0c736f000c5b57fa3c5d1d885e
FeranmiDev <macguzik98@gmail.com>
2024-03-02 17:27:28 +0100

    Refactor Login.vue component

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -14,7 +14,8 @@
 
                 <div class="mb-4">
                     <label for="password" class="block text-gray-700 mb-2">Password</label>
-                    <input v-model="password" type="password" id="password" class="input-field" required minlength="2" />
+                    <input v-model="password" type="password" id="password" class="input-field" required
+                        minlength="2" />
                 </div>
 
                 <div v-if="isLoading" class="text-center py-2">
@@ -29,10 +30,23 @@
                     Login
                 </button>
             </form>
+            <div v-if="isLoading" class="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-50">
+                <div class="bg-white rounded-lg p-6 shadow-xl">
+                    <svg class="animate-spin h-10 w-10 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4">
+                        </circle>
+                        <path class="opacity-75" fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                        </path>
+                    </svg>
+                    <p class="text-center mt-3">Please wait...</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
-  
+
 <script setup>
 import apsLogo from '@/assets/images/aps-logo.png'
 import { ref } from 'vue';
@@ -89,7 +103,7 @@ const handleLogin = async () => {
 }
 
 </script>
-  
+
 <style scoped>
 /* Tailwind CSS Styles */
 
@@ -103,4 +117,3 @@ const handleLogin = async () => {
 
 /* Additional Styles to Customize */
 </style>
-  
